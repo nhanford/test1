60b2e325a7335faba4129318892d2dec4dce19b4
Nate Hanford <nhanford@es.net>
2015-07-13 18:39:08 -0700

    rough test script towards end-system optimization
    patched up a little to work with Ubuntu

--- source/solution.py
+++ source/solution.py
@@ -27,7 +27,7 @@ class CLIError(Exception):
     '''Generic exception to raise and log different fatal errors.'''
     def __init__(self, msg):
         super(CLIError).__init__(type(self))
-        self.msg = "E: %s" % msg
+        self.msg = 'E: %s' % msg
     def __str__(self):
         return self.msg
     def __unicode__(self):
@@ -38,15 +38,16 @@ def database():
     c.execute('''CREATE TABLE conns (command text, pid int, user text, fd text, type text, device text, size text, node text, name text)''')
     conn.commit()
     conn.close()
+    return
 #Here are the functions that poll the proc filesystem, etc.
 def checkibalance():
-    p = subprocess.Popen(["service","irqbalance","status"], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['service','irqbalance','status'], stdout=subprocess.PIPE)
     out,err = p.communicate()
-    if "stop" in out or "inactive" in out:
-        print "irqbalance is off\n"
+    if 'stop' in out or 'inactive' in out:
+        print 'irqbalance is off\n'
         return True
     else:
-        print "Please stop irqblance with \n [sudo] service irqbalance stop\n"
+        print 'Please stop irqblance with \n [sudo] service irqbalance stop\n'
         return False
 
 def pollcpu():
@@ -65,9 +66,9 @@ def pollcpu():
 def pollaffinity(irqlist):
     affinity = dict()
     for i in irqlist:
-        openstring = "/proc/irq/"+i+"/smp_affinity"
-        file = open(openstring)
-        affinity[i]=file.read().strip()
+        file = open('/proc/irq/'+i+'/smp_affinity','r')
+        thisAffinity=file.read().strip()
+        affinity[i]=thisAffinity
     return affinity
 
 def pollirq(iface):
@@ -80,12 +81,21 @@ def pollirq(iface):
             line = re.search('\d+:',line.group(0))
             line = re.search('\d+',line.group(0))
             irqlist.append(line.group(0))
-    file.close()
     if any(irqlist):
         return irqlist
-    else:
-        print "Cannot find this interface's irq numbers."
-        exit()
+    driver = subprocess.check_output(['ethtool','-i',iface])
+    if 'mlx4' in driver:
+        file.seek(0)
+        for line in file:
+            line.strip()
+            line = re.search('.+'+'mlx4',line)
+            if(line):
+                line = re.search('\d+:',line.group(0))
+                line = re.search('\d+',line.group(0))
+                irqlist.append(line.group(0))
+        return irqlist
+    print 'Cannot find this interface\'s irq numbers.'
+    exit()
     #yeah I know that's sloppy; will fix by raising an exception when I get my exception hierarchy figured out.
 
 def setaffinity(affy,numcpus):
@@ -96,32 +106,45 @@ def setaffinity(affy,numcpus):
         if irqcount > numcpus - 1:
             mask = 1
             irqcount = 0
-        strmask = "%x" % mask
+        strmask = '%x' % mask
         while len(strmask)<numdigits:
-            strmask = "0"+strmask
+            strmask = '0'+strmask
         mask = mask << 1
         try:
-            smp = open("/proc/irq/"+key+"/smp_affinity","w")
+            smp = open('/proc/irq/'+key+'/smp_affinity','w')
             smp.write(strmask)
             smp.close()
         except IOError:
-            print("Could not write the smp_affinity file")
+            print 'Could not write the smp_affinity file'
         irqcount +=1
     return
 
+def setperformance(numcpus):
+    for i in range(numcpus):
+        try:
+            throttle = open('/sys/devices/system/cpu/cpu'+i+'/cpufreq/scaling_governor', 'w')
+            throttle.write('performance')
+            throttle.close()
+        except IOError:
+            print 'Could not set CPUs to performance'
+    return
+
 def getlinerate(iface):
-    p = subprocess.Popen(["ethtool",iface], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['ethtool',iface], stdout=subprocess.PIPE)
     out,err = p.communicate()
     speed = re.search('.+Speed:.+',out)
     speed = re.sub('.+Speed:\s','',speed.group(0))
     speed = re.sub('Mb/s','',speed)
-    return speed
+    if 'Unknown' in speed:
+        'Line rate for this interface is unknown: you probably need to enable it.'
+        exit()
+        return speed
 
 def throttleoutgoing(iface,linerate):
     pass
 
 def pollconnections(iface):
-    p = subprocess.Popen(["lsof"," -i :2811"])
+    p = subprocess.Popen(['lsof',' -i :2811'])
     out,err = p.communicate()
     return
 
@@ -136,10 +159,10 @@ def main(argv=None): # IGNORE:C0111
     else:
         sys.argv.extend(argv)
     program_name = os.path.basename(sys.argv[0])
-    program_version = "v%s" % __version__
+    program_version = 'v%s' % __version__
     program_build_date = str(__updated__)
     program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
-    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    program_shortdesc = __import__('__main__').__doc__.split('\n')[1]
     program_license = '''%s
 
 
@@ -149,38 +172,38 @@ USAGE
     try:
         # Setup argument parser
         parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("interface", metavar="interface", action="store", help="specify the interface name of your network controller (i.e. eth1)")
+        parser.add_argument('interface', metavar='interface', action='store', help='specify the interface name of your network controller (i.e. eth1)')
         # Process arguments
         args = parser.parse_args()
         interface = args.interface
 
     except KeyboardInterrupt:
-        print "Operation Cancelled\n"
+        print 'Operation Cancelled\n'
         return 0
     except Exception, e:
         if DEBUG or TESTRUN:
             raise(e)
-        indent = len(program_name) * " "
-        sys.stderr.write(program_name + ": " + repr(e) + "\n")
-        sys.stderr.write(indent + "  for help use --help"+"\n")
+        indent = len(program_name) * ' '
+        sys.stderr.write(program_name + ': ' + repr(e) + '\n')
+        sys.stderr.write(indent + '  for help use --help'+'\n')
         return 2
     if checkibalance():
         numcpus = pollcpu()
-        print "The number of cpus is:", numcpus
+        print 'The number of cpus is:', numcpus
         irqlist = pollirq(interface)
         affinity = pollaffinity(irqlist)
         print(affinity)
         setaffinity(affinity,numcpus)
         linerate = getlinerate(interface)
         print linerate
-        if(int(linerate)<=10000):
-            throttleoutgoing(interface,linerate)
+        throttleoutgoing(interface,linerate)
         database()
         pollconnections(interface)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     if DEBUG:
-        sys.argv.append("-h")
+        sys.argv.append('-h')
     if TESTRUN:
         import doctest
         doctest.testmod()
